c85a4de687e509af890d6b883213fa0998854b5a
justishawkins <32522747+justishawkins@users.noreply.github.com>
2018-01-31 09:41:40 -0500

    bug fixes

--- turtle_stamps.py
+++ turtle_stamps.py
@@ -7,21 +7,25 @@ myturt.shape("square")
 myturt.penup()
 tfood_id = []
 tfood_coord=[]
+
+#if snake.pos() == i:
+    
 for i in range(random.randint(11,36)):
     food = (random.randint(-300,300),random.randint(-300,300))
     myturt.setpos(food[0],food[1])
     this_stamp = myturt.stamp()
     tfood_coord.append((food[0],food[1]))
     tfood_id.append(this_stamp)
-
                
-count = 0               
-for i in tfood_id:
+count = 0
+print(len(tfood_id),len(tfood_coord))
+for i in range(len(tfood_id)):
     print(tfood_coord[count])
     myturt.clearstamp(tfood_id[count])
+    #tfood_id.pop(count)
     count += 1
 
-
+print(myturt.pos())
 
 myturt.setpos(0,0)
 myturt.pendown()
