43f7b708ce5261cb7b146792c652e060eeeb9b7e
justishawkins <32522747+justishawkins@users.noreply.github.com>
2018-02-01 08:13:54 -0500

    class work F

--- chapter-8/classB.py
+++ chapter-8/classB.py
@@ -1,1 +1,31 @@
-s = "Pirates of the Caribbean"
+import sys
+import math
+
+
+def test(did_pass):
+    """  Print the result of a test.  """
+    linenum = sys._getframe(1).f_lineno  # Get the caller's line number.
+    if did_pass:
+        msg = "Test at line {0} ok.".format(linenum)
+    else:
+        msg = ("Test at line {0} FAILED.".format(linenum))
+    print(msg)
+
+
+def find(strng, ch):
+    """
+      Find and return the index of ch in strng.
+      Return -1 if ch does not occur in strng.
+    """
+    count = 0
+    while count < len(strng):
+        tempvar = strng[count]
+        if strng[count] == ch:
+            return count
+        count += 1
+    return -1
+
+test(find("Compsci", "p") == 3)
+test(find("Compsci", "C") == 0)
+test(find("Compsci", "i") == 6)
+test(find("Compsci", "x") == -1)
--- chapter-8/classworkF.py
+++ chapter-8/classworkF.py
@@ -1,11 +1,30 @@
-fruit = "banana"
-ix = 0
-while ix < len(fruit):
-    letter = fruit[ix]
-    print(ix, letter)
-    ix += 1
+import sys
+import math
 
-count = 0
-for c in fruit:
-    print(count,c)
-    count +=1
+
+def test(did_pass):
+    """  Print the result of a test.  """
+    linenum = sys._getframe(1).f_lineno  # Get the caller's line number.
+    if did_pass:
+        msg = "Test at line {0} ok.".format(linenum)
+    else:
+        msg = ("Test at line {0} FAILED.".format(linenum))
+    print(msg)
+
+
+def find(strng, ch):
+    """
+      Find and return the index of ch in strng.
+      Return -1 if ch does not occur in strng.
+    """
+    ix = 0
+    while ix < len(strng):
+        if strng[ix] == ch:
+            return ix
+        ix += 1
+    return -1
+
+test(find("Compsci", "p") == 3)
+test(find("Compsci", "C") == 0)
+test(find("Compsci", "i") == 6)
+test(find("Compsci", "x") == -1)
