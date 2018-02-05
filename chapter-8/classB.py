aa0e183648ef5384c8b93968ed2dfa6b5b99568b
justishawkins <32522747+justishawkins@users.noreply.github.com>
2018-02-05 14:46:20 -0500

    class work B

--- chapter-8/classB.py
+++ chapter-8/classB.py
@@ -1,5 +1,5 @@
 import sys
-import math
+import string
 
 
 def test(did_pass):
@@ -11,18 +11,26 @@ def test(did_pass):
         msg = ("Test at line {0} FAILED.".format(linenum))
     print(msg)
 
-def find2(strng, ch, start=0):
-    ix = start
-    while ix < len(strng):
-        if strng[ix] == ch:
-            return ix
-        ix += 1
-    return -1
+#punctuation = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
 
-print("find2 ",find2("apple","a" ))
-ss = "Python strings  some interesting methods."
-if(ss.find("some",77))>=0:
-    print("have is in the string")
-else:
-    print("no have")
-print(ss.find("have"))
+def remove_punctuation(s):
+    s_sans_punct = ""
+    for letter in s:
+        if letter not in string.punctuation:
+            s_sans_punct += letter
+    return s_sans_punct
+
+my_story = """
+Pythons are constrictors, which means that they will 'squeeze' the life
+out of their prey. They coil themselves around their prey and with
+each breath the creature takes the snake will squeeze a little tighter
+until they stop breathing completely. Once the heart stops the prey
+is swallowed whole. The entire animal is digested in the snake's
+stomach except for fur or feathers. What do you think happens to the fur,
+feathers, beaks, and eggshells? The 'extra stuff' gets passed out as ---
+you guessed it --- snake POOP! """
+
+str='Well, I never did!", sa<id >Alice.'
+
+words=remove_punctuation(my_story).split()
+print(words)
