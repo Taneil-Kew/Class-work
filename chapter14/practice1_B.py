49ea614cf565f3cb8cdb2d0ce993f022fbd19156
justishawkins <32522747+justishawkins@users.noreply.github.com>
2018-05-02 09:57:38 -0400

    merge search

--- chapter14/practice1_B.py
+++ chapter14/practice1_B.py
@@ -70,8 +70,7 @@ def search_binary(xs, target):
         # Fetch the item at that position
         item_at_mid = xs[mid_index]
 
-        # print("ROI[{0}:{1}](size={2}), probed='{3}', target='{4}'"
-        #       .format(lb, ub, ub-lb, item_at_mid, target))
+        #print("ROI[{0}:{1}](size={2}), probed='{3}', target='{4}'".format(lb, ub, ub-lb, item_at_mid, target))
 
         # How does the probed item compare to the target?
         if item_at_mid == target:
@@ -81,6 +80,68 @@ def search_binary(xs, target):
         else:
             ub = mid_index        # Use lower half of ROI next time
 
+def remove_adjacent_dups(xs):
+    """ Return a new list in which all adjacent
+        duplicates from xs have been removed.
+    """
+    result = []
+    most_recent_elem = None
+    for e in xs:
+        if e != most_recent_elem:
+            result.append(e)
+            most_recent_elem = e
+
+    return result
+
+def merge(xs, ys):
+    """ merge sorted lists xs and ys. Return a sorted result """
+    result = []
+    xi = 0
+    yi = 0
+
+    while True:
+        if xi >= len(xs):          # If xs list is finished,
+            result.extend(ys[yi:]) # Add remaining items from ys
+            return result          # And we're done.
+
+        if yi >= len(ys):          # Same again, but swap roles
+            result.extend(xs[xi:])
+            return result
+
+        # Both lists still have items, copy smaller item to result.
+        if xs[xi] <= ys[yi]:
+            result.append(xs[xi])
+            xi += 1
+        else:
+            result.append(ys[yi])
+            yi += 1
+
+def find_unknowns_merge_pattern(vocab, wds):
+    """ Both the vocab and wds must be sorted.  Return a new
+        list of words from wds that do not occur in vocab.
+    """
+
+    result = []
+    xi = 0
+    yi = 0
+
+    while True:
+        if xi >= len(vocab):
+            result.extend(wds[yi:])
+            return result
+
+        if yi >= len(wds):
+            return result
+
+        if vocab[xi] == wds[yi]:  # Good, word exists in vocab
+            yi += 1
+
+        elif vocab[xi] < wds[yi]: # Move past this vocab word,
+            xi += 1
+
+        else:                     # Got word that is not in vocab
+            result.append(wds[yi])
+            yi += 1
 
 bigger_vocab = load_words_from_file("vocab.txt")
 print("There are {0} words in the vocab, starting with\n {1} "
@@ -111,3 +172,34 @@ print("There are {0} unknown words.".format(len(missing_words)))
 print("That took {0:.4f} seconds.".format(t1-t0))
 
 
+test(remove_adjacent_dups([1,2,3,3,3,3,5,6,9,9]) == [1,2,3,5,6,9])
+test(remove_adjacent_dups([]) == [])
+test(remove_adjacent_dups(["a", "big", "big", "bite", "dog"]) ==["a", "big", "bite", "dog"])
+
+all_words = get_words_in_book("alice_in_wonderland.txt")
+all_words.sort()
+book_words = remove_adjacent_dups(all_words)
+print("There are {0} words in the book. Only {1} are unique.".
+                      format(len(all_words), len(book_words)))
+print("The first 100 words are\n{0}".
+           format(book_words[:100]))
+
+xs = [1,3,5,7,9,11,13,15,17,19]
+ys = [4,8,12,16,20,24]
+zs = xs+ys
+zs.sort()
+test(merge(xs, []) == xs)
+test(merge([], ys) == ys)
+test(merge([], []) == [])
+test(merge(xs, ys) == zs)
+test(merge([1,2,3], [3,4,5]) == [1,2,3,3,4,5])
+test(merge(["a", "big", "cat"], ["big", "bite", "dog"]) == ["a", "big", "big", "bite", "cat", "dog"])
+
+all_words = get_words_in_book("alice_in_wonderland.txt")
+t0 = time.clock()
+all_words.sort()
+book_words = remove_adjacent_dups(all_words)
+missing_words = find_unknowns_merge_pattern(bigger_vocab, book_words)
+t1 = time.clock()
+print("There are {0} unknown words.".format(len(missing_words)))
+print("That took {0:.4f} seconds.".format(t1-t0))
